4cf93fbc78b5874a12a98730e4a0b37bb54b349e
Evgenii Novikov <enovikov11@yandex.com>
2025-02-16 09:47:33 +0100

    oai

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from collections import defaultdict
-from ollama import Client
+# from ollama import Client
+from openai import OpenAI
 from tqdm import tqdm
+from time import time
 import json
 
+runner = None
 
-# Config
-models = ["deepseek-r1:70b", "llama3.3:70b", "dolphin-mixtral:8x7b", "qwen2.5:32b", "command-r:latest"]
-client = Client(host="http://127.0.0.1:11434")
+# Config Ollama
+# models = ["deepseek-r1:70b", "llama3.3:70b", "dolphin-mixtral:8x7b", "qwen2.5:32b", "command-r:latest"]
+# client = Client(host="http://127.0.0.1:11434")
+# query = lambda model, prompt: client.chat(model=model, messages=[{"role": "user", "content": prompt}]).model_dump()
 
+# Config OAI
+models = ["gpt-4o", "o1"]
+client = OpenAI()
+query = lambda model, prompt: client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt}]).model_dump()
 
 # File storage
 with open("in-prompts.json", "r") as file:
@@ -19,7 +27,7 @@ with open("out-results.json.log", "r") as file:
     for line in file:
         data = json.loads(line)
 
-        if "result" in data:
+        if "result" in data and data.get("runner", None) == runner:
             done[data["model"]].add(data["task_id"])
 
 log = open("out-results.json.log", "a")
@@ -32,10 +40,13 @@ for i, model in enumerate(models):
     unsolved_tasks = [task for task in tasks if task[0] not in done[model]]
 
     for task_id, prompt in tqdm(unsolved_tasks):
-        entry = {"model": model, "task_id": task_id}
+        entry = {"model": model, "task_id": task_id, "runner": runner}
 
         try:
-            entry["result"] = client.chat(model=model, messages=[{"role": "user", "content": prompt}]).model_dump()
+            started = time()
+            entry["result"] = query(model, prompt)
+            entry["elapsed_time"] = time() - started
+            
         except Exception as e:
             entry["error"] = str(e)
 

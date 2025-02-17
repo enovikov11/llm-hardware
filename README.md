d9215a386ebf159a1549d658a9625f6da3c4b499
Evgenii Novikov <enovikov11@yandex.com>
2025-02-17 17:57:04 +0100

    prompts

--- README.md
+++ README.md
@@ -151,6 +151,6 @@ ollama pull dolphin-mixtral:8x7b  # 26 GB
 ollama pull deepseek-r1:70b       # 42 GB
 ollama pull llama3.3:70b          # 42 GB
 
-cd /Users/enovikov11/Desktop/monorepo/github/mac-ai-bench ; python3.11 main.py --runner  --url  --user vastai --pwd  ; afplay /Users/enovikov11/Downloads/mon.mp3
+cd /Users/enovikov11/Desktop/monorepo/github/mac-ai-bench ; python3.11 main.py --runner  --ignoressl --url  --user vastai --pwd  ; afplay /Users/enovikov11/Downloads/mon.mp3
 
 /Users/enovikov11/Library/Python/3.11/bin/vastai destroy instance 17965232
--- main.py
+++ main.py
@@ -10,27 +10,26 @@ import json
 import os
 
 
-# Parse args
 parser = argparse.ArgumentParser(description="AI benchmark")
 parser.add_argument("--runner", required=True, type=str, help="Your rig codename")
 parser.add_argument("--models", type=str, help="Comma-separated list of models", default="llama2-uncensored:7b,deepseek-r1:7b,qwen2.5:14b,phi4:14b,deepseek-r1:32b,qwen2.5:32b,command-r:latest,dolphin-mixtral:8x7b,deepseek-r1:70b,llama3.3:70b")
-parser.add_argument("--allowlist", type=str, help="Comma-separated list of prompts ids to run", default="2,255,4,135,8,137,138,136,7,269,397,15,17,18,276,21,152,286,159,160,161,164,46,181,196,203,209,84,85,217,352,230,105,235,239,246,249,378,251,127")
-parser.add_argument("--ollama", action="store_true", help="Use Ollama API for inference (default)")
+parser.add_argument("--allowlist", type=str, help="Comma-separated list of prompts ids to run or \"all\"", default="2,255,4,135,8,137,138,136,7,269,397,15,17,18,276,21,152,286,159,160,161,164,46,181,196,203,209,84,85,217,352,230,105,235,239,246,249,378,251,127")
 parser.add_argument("--openai", action="store_true", help="Use OpenAI API for inference")
-parser.add_argument("--url", type=str, help="Ollama url (ignores ssl cert)")
-parser.add_argument("--user", type=str, help="Ollama url http user (ignores ssl cert)")
-parser.add_argument("--pwd", type=str, help="Ollama url http pass (ignores ssl cert)")
+parser.add_argument("--url", type=str, help="Ollama URL")
+parser.add_argument("--user", type=str, help="HTTP user")
+parser.add_argument("--pwd", type=str, help="HTTP password")
+parser.add_argument("--ignoressl", action="store_true", help="Ignore ssl cert (insecure)")
 args = parser.parse_args()
 
-runner = args.runner
+runner = "oai" if args.openai else args.runner
 models = args.models.split(',')
-allowlist = [int(num) for num in args.allowlist.split(',')] if args.allowlist else None
+allowlist = [int(num) for num in args.allowlist.split(',')] if args.allowlist and args.allowlist != "all" else None
 
 if args.openai:
     inference = OpenAI().chat
 elif args.url:
     auth_header = f"Basic {b64encode(f'{args.user}:{args.pwd}'.encode()).decode()}"
-    transport = httpx.HTTPTransport(verify=False)
+    transport = httpx.HTTPTransport(verify=not args.ignoressl)
     http_client = httpx.Client(transport=transport, headers={"Authorization": auth_header})
 
     client = Client(host=args.url)
@@ -40,15 +39,8 @@ elif args.url:
 else:
     inference = Client(host="http://127.0.0.1:11434").chat
 
-if args.openai:
-    runner = "oai"
-
-
-# File storage
-with open("in-prompts.json", "r") as file:
-    tasks = list(enumerate(json.load(file)))
-
 done = defaultdict(set)
+tasks = []
 
 for file in os.listdir("."):
     if not file.endswith(".json.log"):
@@ -60,6 +52,9 @@ for file in os.listdir("."):
 
             if "result" in data and data.get("runner", None) == runner:
                 done[data["model"]].add(data["task_id"])
+            
+            if "prompt" in data and "task_id" in data:
+                tasks.append((data["task_id"], data["prompt"]))
 
 log = open(f"{int(time())}.json.log", "a")
 

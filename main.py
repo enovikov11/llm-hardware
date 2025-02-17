18972f73d85522e81d2b279540a83fba70ef8940
Evgenii Novikov <enovikov11@yandex.com>
2025-02-17 20:54:04 +0100

    main compatible with format

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import os
 parser = argparse.ArgumentParser(description="AI benchmark")
 parser.add_argument("--runner", required=True, type=str, help="Your rig codename")
 parser.add_argument("--models", type=str, help="Comma-separated list of models", default="llama2-uncensored:7b,deepseek-r1:7b,qwen2.5:14b,phi4:14b,deepseek-r1:32b,qwen2.5:32b,command-r:latest,dolphin-mixtral:8x7b,deepseek-r1:70b,llama3.3:70b")
-parser.add_argument("--allowlist", type=str, help="Comma-separated list of prompts ids to run or \"all\"", default="2,255,4,135,8,137,138,136,7,269,397,15,17,18,276,21,152,286,159,160,161,164,46,181,196,203,209,84,85,217,352,230,105,235,239,246,249,378,251,127")
+parser.add_argument("--prompts", type=str, help="main, all or comma-separated list of prompts ids", default="main")
 parser.add_argument("--openai", action="store_true", help="Use OpenAI API for inference")
 parser.add_argument("--url", type=str, help="Ollama URL")
+parser.add_argument("--ignoressl", action="store_true", help="Ignore ssl cert (insecure)")
 parser.add_argument("--user", type=str, help="HTTP user")
 parser.add_argument("--pwd", type=str, help="HTTP password")
-parser.add_argument("--ignoressl", action="store_true", help="Ignore ssl cert (insecure)")
 args = parser.parse_args()
 
 runner = "oai" if args.openai else args.runner
 models = args.models.split(",")
-allowlist = [int(num) for num in args.allowlist.split(",")] if args.allowlist and args.allowlist != "all" else None
+prompts_filter = args.prompts if args.prompts in ("main", "all") else [int(n) for n in args.prompts.split(",")]
 
 if args.openai:
     inference = OpenAI().chat
@@ -48,20 +48,19 @@ for file in os.listdir("."):
 
     with open(file, "r") as file:
         for line in file:
-            data = json.loads(line)
+            item = json.loads(line)
 
-            if "result" in data and data.get("runner", None) == runner:
-                done[data["model"]].add(data["prompt_id"])
-            
-            if "prompt" in data and "prompt_id" in data:
-                prompts.append(data)
+            if item["type"] == "run" and "result" in item and item["runner"] == runner:
+                done[item["model"]].add(item["prompt_id"])
+            elif item["type"] == "prompt" and \
+            (prompts_filter == "all" or (item.get("main") if prompts_filter == "main" else item["prompt_id"] in prompts_filter)):
+                prompts.append(item)
 
 log = open(f"{int(time())}.json.log", "a")
 
 for i, model in enumerate(models):
     print(f"Model {model} ({i + 1}/{len(models)})")
-
-    tasks = [prompt for prompt in prompts if prompt["prompt_id"] not in done[model] and (allowlist == None or prompt["prompt_id"] in allowlist)]
+    tasks = [prompt for prompt in prompts if prompt["prompt_id"] not in done[model]]
 
     for task in tqdm(tasks):
         entry = {"type": "run", "runner": runner, "model": model, "prompt_id": task["prompt_id"]}
